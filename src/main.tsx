0b8195f936b3e4186390146837f434042584e211
Yunior C. Fonseca Reyna <34970661+fonsecareyna82@users.noreply.github.com>
2026-05-27 12:00:16 +0200

    load protocol dark mode overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
+import "./components/protocol/protocol-dark-overrides.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
 
